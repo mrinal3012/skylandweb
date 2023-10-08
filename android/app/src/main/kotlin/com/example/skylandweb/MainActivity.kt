95cc28ee6db4b9e8d759f1ec12e401791d1594dc
https://github.com/mrinal3012 <mrinalpalit.cse@gmail.com>
2023-10-08 10:58:34 +0600

    add screen page

---
+++ android/app/src/main/kotlin/com/example/skylandweb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skylandweb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/screen_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScreenPage extends StatefulWidget {
+  const ScreenPage({super.key});
+
+  @override
+  State<ScreenPage> createState() => _ScreenPageState();
+}
+
+class _ScreenPageState extends State<ScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
