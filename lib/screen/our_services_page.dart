0afc581930326295399b92425ccc6db2e00f82d8
https://github.com/mrinal3012 <mrinalpalit.cse@gmail.com>
2023-10-08 12:42:52 +0600

    add srevices and project page

---
+++ lib/screen/our_services_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OurServicesPage extends StatefulWidget {
+  const OurServicesPage({super.key});
+
+  @override
+  State<OurServicesPage> createState() => _OurServicesPageState();
+}
+
+class _OurServicesPageState extends State<OurServicesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
