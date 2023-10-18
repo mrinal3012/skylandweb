a65d42e72018f906724ba688ae96db46e8215efe
https://github.com/mrinal3012 <mrinalpalit.cse@gmail.com>
2023-10-18 16:42:34 +0600

    add blog

---
+++ lib/screen/our_blog_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OurBlogPage extends StatefulWidget {
+  const OurBlogPage({super.key});
+
+  @override
+  State<OurBlogPage> createState() => _OurBlogPageState();
+}
+
+class _OurBlogPageState extends State<OurBlogPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
